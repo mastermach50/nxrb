a216b6d62f54d8da84be7763c7981fa18f4178c9
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-04-25 19:53:40 +0530

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A nixos build helper";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      cargoToml = fromTOML (builtins.readFile ./Cargo.toml);
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit system;
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
+    };
+}
